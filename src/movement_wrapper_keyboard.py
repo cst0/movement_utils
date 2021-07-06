b54a7d36c6c7761c851625f094afe9dac49d0399
Chris Thierauf <christopher.thierauf@tufts.edu>
2021-07-06 12:54:49 -0400

    reformat

--- src/movement_wrapper_keyboard.py
+++ src/movement_wrapper_keyboard.py
@@ -6,12 +6,14 @@ import termios, select, sys, tty
 from nav_msgs.msg import Odometry
 from movement_utils.srv import GoToRelative, GoToRelativeRequest
 from movement_utils.srv import LinearVel, LinearVelRequest
-GOTO_RELATIVE_CLIENT
-LINEAR_VEL_CLIENT
-ODOM_SUB
-CURRENT_ODOM
+
+GOTO_RELATIVE_CLIENT = None
+LINEAR_VEL_CLIENT = None
+ODOM_SUB = None
+CURRENT_ODOM = None
 USAGE = """
 Make and send service calls to the movement wrapper utility.
+
 Q/A: Increase/Decrease linear speed
 W/S: Increase/Decrease angular speed
 E/D: Increase/Decrease linear travel
@@ -26,58 +28,83 @@ I:   Send LinearVel request based off of default parameters
 """
 RUNNING_CMD = "Ready"
 
+
 class validRequests(Enum):
-    GoToRelativeZero=0
-    LinearVelZero=1
-    GoToRelativeCustom=2
-    LinearVelCustom=3
-    GoToRelativeDefault=4
-    LinearVelDefault=5
+    GoToRelativeZero = 0
+    LinearVelZero = 1
+    GoToRelativeCustom = 2
+    LinearVelCustom = 3
+    GoToRelativeDefault = 4
+    LinearVelDefault = 5
+
 
 messageSendBindings = {
-        'z':validRequests.GoToRelativeZero,
-        'x':validRequests.LinearVelZero,
-        'j':validRequests.GoToRelativeCustom,
-        'k':validRequests.LinearVelCustom,
-        'u':validRequests.GoToRelativeDefault,
-        'i':validRequests.LinearVelDefault,
-    }
+    "z": validRequests.GoToRelativeZero,
+    "x": validRequests.LinearVelZero,
+    "j": validRequests.GoToRelativeCustom,
+    "k": validRequests.LinearVelCustom,
+    "u": validRequests.GoToRelativeDefault,
+    "i": validRequests.LinearVelDefault,
+}
 
-messageModLegends ={
-        'q':"increase linear speed",
-        'a':"decrease linear speed",
-        'w':"increase angular speed",
-        's':"decrease angular speed",
-        'e':"increase linear travel",
-        'd':"decrease linear travel",
-        'r':"increase angular travel",
-        'f':"decrease angular travel",
-    }
+messageModLegends = {
+    "q": "increase linear speed",
+    "a": "decrease linear speed",
+    "w": "increase angular speed",
+    "s": "decrease angular speed",
+    "e": "increase linear travel",
+    "d": "decrease linear travel",
+    "r": "increase angular travel",
+    "f": "decrease angular travel",
+}
 
-messageModBindings={
-        'q': (1.1 , 0   , 0   , 0   , 0   , 0   , 0   , 0 )   ,
-        'a': (0   , 0.9 , 0   , 0   , 0   , 0   , 0   , 0 )   ,
-        'w': (0   , 0   , 1.1 , 0   , 0   , 0   , 0   , 0 )   ,
-        's': (0   , 0   , 0   , 0.9 , 0   , 0   , 0   , 0 )   ,
-        'e': (0   , 0   , 0   , 0   , 1.1 , 0   , 0   , 0 )   ,
-        'd': (0   , 0   , 0   , 0   , 0   , 0.9 , 0   , 0 )   ,
-        'r': (0   , 0   , 0   , 0   , 0   , 0   , 1.1 , 0 )   ,
-        'f': (0   , 0   , 0   , 0   , 0   , 0   , 0   , 0.9 ) ,
-    }
+messageModBindings = {
+    "q": (1.1, 0, 0, 0, 0, 0, 0, 0),
+    "a": (0, 0.9, 0, 0, 0, 0, 0, 0),
+    "w": (0, 0, 1.1, 0, 0, 0, 0, 0),
+    "s": (0, 0, 0, 0.9, 0, 0, 0, 0),
+    "e": (0, 0, 0, 0, 1.1, 0, 0, 0),
+    "d": (0, 0, 0, 0, 0, 0.9, 0, 0),
+    "r": (0, 0, 0, 0, 0, 0, 1.1, 0),
+    "f": (0, 0, 0, 0, 0, 0, 0, 0.9),
+}
+
 
 def getKey(key_timeout):
-    """ stolen straight from teleop_twist_keyboard """
+    """stolen straight from teleop_twist_keyboard"""
     tty.setraw(sys.stdin.fileno())
     rlist, _, _ = select.select([sys.stdin], [], [], key_timeout)
     if rlist:
         key = sys.stdin.read(1)
     else:
-        key = ''
+        key = ""
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
     return key
 
+
 def odom_callback(msg: Odometry):
     CURRENT_ODOM = msg
+
+
+def sendMessage(key):
+    pass
+    # messageSendBindings.get(
+
+
+##= {
+##        'z':validRequests.GoToRelativeZero,
+##        'x':validRequests.LinearVelZero,
+##        'j':validRequests.GoToRelativeCustom,
+##        'k':validRequests.LinearVelCustom,
+##        'u':validRequests.GoToRelativeDefault,
+##        'i':validRequests.LinearVelDefault,
+##    }
+
+
+def modMessage(key):
+
+    pass
+
 
 def main():
     key_timeout = rospy.get_param("~key_timeout", 0.0)
@@ -88,31 +115,34 @@ def main():
         "movement_wrapper/goto_relative", GoToRelative
     )
     global LINEAR_VEL_CLIENT
-    LINEAR_VEL_CLIENT = rospy.ServiceProxy(
-        "movement_wrapper/linear_vel", LinearVel
-    )
+    LINEAR_VEL_CLIENT = rospy.ServiceProxy("movement_wrapper/linear_vel", LinearVel)
     global ODOM_SUB
     global CURRENT_ODOM
     ODOM_SUB = rospy.Subscriber("odom", Odometry, odom_callback)
     CURRENT_ODOM = Odometry()
+
     try:
         print(USAGE)
         print(RUNNING_CMD)
         while not rospy.is_shutdown():
             key = getKey(key_timeout)
-            print(key)
+            if key == "\x03":
+                # ctrl-c, abort
+                raise KeyboardInterrupt
             if key.lower() in messageSendBindings.keys():
-
-                pass
+                sendMessage(key.lower())
             elif key.lower() in messageModBindings.keys():
-                pass
+                modMessage(key.lower())
+
+    except KeyboardInterrupt:
+        rospy.loginfo("told to shutdown, goodbye!")
+        rospy.signal_shutdown("caught ctrl-c")
 
     except Exception as e:
         print(e)
 
     finally:
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-
 
     pass
 
